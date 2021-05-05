44ca3edc6238b861259e3c5d17582501374e094b
Yeff <76024824+YeffyCodeGit@users.noreply.github.com>
2021-05-05 15:13:50 +0600

    e

---
+++ Blacknight/Services/ConfigurationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Blacknight.Services
+{
+    class ConfigurationService
+    {
+        public static string GetClientToken()
+        {
+            return File.ReadAllText(@"C:\Users\aditc\Desktop\Programms\Blacknight\Blacknight\token.txt");;
+        }
+    }
+}
---
+++ Blacknight/Services/LoggingService.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blacknight.Logging
+{
+    class LoggingService
+    {
+       public LoggingService(DiscordSocketClient client)
+       {
+            client.Log += LogAsync;
+       }
+
+       private Task LogAsync(LogMessage msg)
+       {
+            if (msg.Severity == LogSeverity.Info) Console.ForegroundColor = ConsoleColor.Green;
+            if (msg.Severity == LogSeverity.Error) Console.ForegroundColor = ConsoleColor.Red;
+            if (msg.Severity == LogSeverity.Critical) Console.ForegroundColor = ConsoleColor.DarkRed;
+            if (msg.Severity == LogSeverity.Warning) Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine(msg.ToString() + $"    [Severity: {msg.Severity}]");
+
+            return Task.CompletedTask;
+       }
+    }
+}
